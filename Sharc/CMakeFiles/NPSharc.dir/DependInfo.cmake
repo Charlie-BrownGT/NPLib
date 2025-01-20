2cc0e5358bc55ef690cb617dbe0314a4bc05ac74
Charlie-BrownGT <108026345+Charlie-BrownGT@users.noreply.github.com>
2025-01-20 16:35:21 +0000

    Add files via upload

---
+++ Sharc/CMakeFiles/NPSharc.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/guy/software/nptool/nptool/NPLib/Detectors/Sharc/TSharcData.cxx" "Detectors/Sharc/CMakeFiles/NPSharc.dir/TSharcData.cxx.o" "gcc" "Detectors/Sharc/CMakeFiles/NPSharc.dir/TSharcData.cxx.o.d"
+  "/home/guy/software/nptool/nptool/NPLib/Detectors/Sharc/TSharcDataDict.cxx" "Detectors/Sharc/CMakeFiles/NPSharc.dir/TSharcDataDict.cxx.o" "gcc" "Detectors/Sharc/CMakeFiles/NPSharc.dir/TSharcDataDict.cxx.o.d"
+  "/home/guy/software/nptool/nptool/NPLib/Detectors/Sharc/TSharcPhysics.cxx" "Detectors/Sharc/CMakeFiles/NPSharc.dir/TSharcPhysics.cxx.o" "gcc" "Detectors/Sharc/CMakeFiles/NPSharc.dir/TSharcPhysics.cxx.o.d"
+  "/home/guy/software/nptool/nptool/NPLib/Detectors/Sharc/TSharcPhysicsDict.cxx" "Detectors/Sharc/CMakeFiles/NPSharc.dir/TSharcPhysicsDict.cxx.o" "gcc" "Detectors/Sharc/CMakeFiles/NPSharc.dir/TSharcPhysicsDict.cxx.o.d"
+  "/home/guy/software/nptool/nptool/NPLib/Detectors/Sharc/TSharcSpectra.cxx" "Detectors/Sharc/CMakeFiles/NPSharc.dir/TSharcSpectra.cxx.o" "gcc" "Detectors/Sharc/CMakeFiles/NPSharc.dir/TSharcSpectra.cxx.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/guy/software/nptool/nptool/NPLib/Core/CMakeFiles/NPCore.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Sharc/CMakeFiles/NPSharc.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "../../lib/libNPSharc.pdb"
+  "../../lib/libNPSharc.so"
+  "CMakeFiles/NPSharc.dir/TSharcData.cxx.o"
+  "CMakeFiles/NPSharc.dir/TSharcData.cxx.o.d"
+  "CMakeFiles/NPSharc.dir/TSharcDataDict.cxx.o"
+  "CMakeFiles/NPSharc.dir/TSharcDataDict.cxx.o.d"
+  "CMakeFiles/NPSharc.dir/TSharcPhysics.cxx.o"
+  "CMakeFiles/NPSharc.dir/TSharcPhysics.cxx.o.d"
+  "CMakeFiles/NPSharc.dir/TSharcPhysicsDict.cxx.o"
+  "CMakeFiles/NPSharc.dir/TSharcPhysicsDict.cxx.o.d"
+  "CMakeFiles/NPSharc.dir/TSharcSpectra.cxx.o"
+  "CMakeFiles/NPSharc.dir/TSharcSpectra.cxx.o.d"
+  "TSharcDataDict.cxx"
+  "TSharcPhysicsDict.cxx"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NPSharc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
